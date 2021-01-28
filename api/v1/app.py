64dcc45c2ca90cb35524ccf48f7f730b8c6ced20
seObando19 <sebastiano-10@hotmail.com>
2021-01-27 22:08:20 -0500

    Completed task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Documentation
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -16,6 +16,14 @@ def teardown_api(exception):
     """closes the storage on teardown"""
     storage.close()
 
+
+@app.errorhandler(404)
+def pega_not_found(e):
+    """
+    create a handler for 404 errors that returns a
+    JSON-formatted 404 status
+    """
+    return jsonify({"error": "Not found"})
 
 if __name__ == '__main__':
     app.run(host=(getenv('HBNB_API_HOST', '0.0.0.0')),
