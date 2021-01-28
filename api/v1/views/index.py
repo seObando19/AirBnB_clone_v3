c63e2d9b32938bcd49fdf0dfd56e3dbf63399c9d
seObando19 <sebastiano-10@hotmail.com>
2021-01-27 20:55:21 -0500

    Process test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,18 @@ from models import storage
 def status():
     """  that returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Create an endpoint that retrieves the number of
+    each objects by type
+    """
+    stats = {"amenities": storage.count('Amenity'),
+             "cities": storage.count('City'),
+             "places": storage.count('Place'),
+             "reviews": storage.count('Review'),
+             "states": storage.count('State'),
+                 "users": storage.count('User')}
+
+    return jsonify(stats)
