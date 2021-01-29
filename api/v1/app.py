722bbf42760b43c4f1813029b220322141b1abd2
seObando19 <sebastiano-10@hotmail.com>
2021-01-28 23:27:37 -0500

    completed task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={"/*": {"origins":"0.0.0.0"}})
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
